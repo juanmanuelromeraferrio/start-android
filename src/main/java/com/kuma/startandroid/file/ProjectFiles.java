413c1bb3d8309a114928c7c1fd90c9a9780a9688
Juan Manuel Romera Ferrio <juan.romera@mercadolibre.com>
2017-06-01 16:46:20 -0300

    refactor

---
+++ src/main/java/com/kuma/startandroid/file/ProjectFiles.java
@@ -0,0 +1,97 @@
+package com.kuma.startandroid.file;
+
+import com.kuma.startandroid.error.BuildProjectException;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Collection;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Stream;
+
+/**
+ * Created by jromera on 1/6/17.
+ */
+public class ProjectFiles {
+
+    public static final String SLASH = "/";
+    public static final String DOT = ".";
+    public static final String DASH = "-";
+
+    public static void createDirectories(Path path) throws BuildProjectException {
+        if (!Files.exists(path)) {
+            try {
+                Files.createDirectories(path);
+            } catch (IOException e) {
+                throw new BuildProjectException(e.getMessage());
+            }
+        } else {
+            throw new BuildProjectException("Error creating project");
+        }
+    }
+
+    public static void copyAndTransform(String source, String target, Map<String, String> transform, Set<String> filters) {
+        Path originPath = Paths.get(source);
+        try {
+            Files.list(originPath).forEach(
+                    (Path file) -> {
+                        boolean isDirectory = Files.isDirectory(file);
+                        if (isDirectory) {
+                            Path targetPath =
+                                    Paths.get(target + SLASH + ProjectFiles.getLastFolderOfPath(file));
+                            if (!Files.exists(targetPath)) {
+                                try {
+                                    Files.createDirectories(targetPath);
+                                } catch (Exception e) {
+                                    throw new BuildProjectException("Error creating project");
+                                }
+                            }
+
+                            copyAndTransform(file.toString(), targetPath.toString(), transform, filters);
+
+                        } else {
+                            Stream<String> input = null;
+                            PrintWriter output = null;
+                            try {
+                                String extension = getExtension(file);
+                                if (filters.contains(extension)) {
+                                    input = Files.lines(file);
+                                    output = new PrintWriter(target + SLASH + file.getFileName().toString());
+                                    input.map(s -> {
+                                                Collection<String> values = transform.keySet();
+                                                for (String key : values) {
+                                                    s = s.replaceAll(key, transform.get(key));
+                                                }
+                                                return s;
+                                            }
+                                    ).forEach(output::println);
+                                } else {
+                                    Files.copy(file, Paths.get(target + SLASH + file.getFileName().toString()));
+                                }
+                            } catch (Exception e) {
+                                throw new BuildProjectException("Error creating project");
+                            } finally {
+                                if (input != null)
+                                    input.close();
+                                if (output != null)
+                                    output.close();
+                            }
+                        }
+                    });
+        } catch (IOException e) {
+            throw new BuildProjectException("Error creating project");
+        }
+    }
+
+    private static String getLastFolderOfPath(Path path) {
+        return path.toString().substring(path.toString().lastIndexOf(SLASH) + 1);
+    }
+
+    private static String getExtension(Path path) {
+        return path.toString().substring(path.toString().lastIndexOf(DOT) + 1,
+                path.toString().length());
+    }
+}
--- src/main/java/com/kuma/startandroid/process/task/CopyAndTransformFiles.java
+++ src/main/java/com/kuma/startandroid/process/task/CopyAndTransformFiles.java
@@ -1,73 +1,28 @@
 package com.kuma.startandroid.process.task;
 
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.stream.Stream;
-
 import com.kuma.startandroid.error.BuildProjectException;
+import com.kuma.startandroid.file.ProjectFiles;
 import com.kuma.startandroid.process.AndroidProjectConfiguration;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
 
 public class CopyAndTransformFiles implements CreateProjectTask {
 
-  @Override
-  public void execute(AndroidProjectConfiguration configuration) throws BuildProjectException {
+    @Override
+    public void execute(AndroidProjectConfiguration configuration) throws BuildProjectException {
+        Map<String, String> transform = new HashMap<>();
+        transform.put("a", "@");
 
-    copyAndTransform(configuration.getResourcesProperties().getSourceProjectFolderPath(),
-        configuration.getResultPath(), "a", "x");
-  }
+        Set<String> filters = new HashSet<>();
+        filters.add("properties");
+        filters.add("java");
+        filters.add("xml");
 
-  private void copyAndTransform(String origin, String destination, String original, String replace) {
-
-    Path originPath = Paths.get(origin);
-    try {
-      Files.list(originPath).forEach(
-          file -> {
-            boolean isDirectory = Files.isDirectory(file);
-            if (isDirectory) {
-              Path path =
-                  Paths.get(destination + "/"
-                      + file.toString().substring(file.toString().lastIndexOf("\\") + 1));
-              if (!Files.exists(path)) {
-                try {
-                  Files.createDirectories(path);
-                } catch (Exception e) {
-                  throw new BuildProjectException("Error creating project");
-                }
-              }
-
-              copyAndTransform(file.toString(), path.toString(), original, replace);
-
-            } else {
-              Stream<String> input = null;
-              PrintWriter output = null;
-              try {
-
-                String extension =
-                    file.toString().substring(file.toString().lastIndexOf(".") + 1,
-                        file.toString().length());
-                if (extension.equals("java")) {
-                  input = Files.lines(file);
-                  output = new PrintWriter(destination + "/" + file.getFileName().toString());
-                  input.map(s -> s.replaceAll(original, replace)).forEach(output::println);
-                } else {
-                  Files.copy(file, Paths.get(destination + "/" + file.getFileName().toString()));
-                }
-              } catch (Exception e) {
-                throw new BuildProjectException("Error creating project");
-              } finally {
-                if (input != null)
-                  input.close();
-                if (output != null)
-                  output.close();
-              }
-            }
-          });
-    } catch (IOException e) {
-      throw new BuildProjectException("Error creating project");
+        ProjectFiles.copyAndTransform(configuration.getResourcesProperties().getSourceProjectFolderPath(),
+                configuration.getResultPath(),
+                transform, filters);
     }
-
-  }
 }
